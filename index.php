a0fffbba872b7f3cd8efe736aa4a904c274ff532
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-03-05 15:24:07 +0100

    html et js entierement refait , achat de click et click incrément fait fonctionne, dossier images avec image ajouté, achat auto click et localstorage a faire

--- index.php
+++ index.php
@@ -1,32 +1,41 @@
-<?php
 
-?>
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/css/style.css">
-    <title>Clicker</title>
+    <script defer src="src/js/script.js"></script>
+    <title>Paimon clicker</title>
 </head>
-<body>
-    <?php session_start(); include("header.php");?>
+    <?php session_start(); include("header.php");?> 
     <main>
         <section>
-            <h1>Bienvenu sur le clicker</h1>
-            <h2 id="compteur"></h2>
-            <button class="clk"><a href="#">click sur ce boutton</a></button>
-        </section>
-        <section id="boutique">
-            <div id="">
-
+            <div class="container-clicker">
+                <h2 id="compteur"></h2>
+                <img src="src/images/paimon.webp" class="img-click" id="clicker" alt="">
+                <p id="message"></p>
+                <div class="container-shop">
+                    <div class="prices-bonus-click-card">
+                        <p>Bonus click +<span id="level-bonus-click"></span></p>
+                        <p>prix : <span id="display-price-bonus-click"></span></p>
+                        <a id="btn-buy-bonus-click" class="myButton">Buy</a>
+                    </div>
+                    <div class="prices-auto-click-card">
+                        <p>Bonus click +<span id="level-auto-click"></span>1</p>
+                        <p>prix : <span id="display-price-auto-click"></span></p>
+                        <a id="btn-buy-auto-click" class="myButton">Buy</a>
+                    </div>
+                    <div class="reset-card">
+                        <p>Réinitialisé le clicker</p>
+                        <a id="btn-reset" class="myButton">Reset</a>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
-    <footer>
-        
-        </footer>
-        <script src="src/js/script.js"></script>
+<body>
+    
 </body>
 </html>
--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,82 @@
-//initilise le compteur
+//les variable d'initialisation
 let count = 0;
+let bonusClickPrice = 100;
+let autoClickPrice = 200;
+let lvlBonusClickPlus = 0;
+let lvlAutoClickPlus = 0;
+let bonusActive = 0;
+
+console.log(bonusActive)
+//selecteurs 
+let counter = document.querySelector("#compteur")
+let clicker = document.querySelector("#clicker")
+//let messages = document.querySelector("#message")
+let displayBonusClickPlus = document.querySelector("#level-bonus-click")
+let displayAutoClickPlus = document.querySelector("#level-auto-click")
+let displayBonusClick = document.querySelector("#display-price-bonus-click")
+let displayAutoClick = document.querySelector("#display-price-auto-click")
+let btnBuyBonusClick = document.querySelector("#btn-buy-bonus-click")
+let btnBuyAutoClick = document.querySelector("#btn-buy-auto-click")
+let btnReset = document.querySelector("#reset")
 
 
-if(typeof(localStorage) !== "undefined") {
-   // let displayCount = document.querySelector('#compteur');
-    count = parseInt(localStorage.getItem("count")) || 0;
+//afficheur de base
+counter.innerText = count;
+displayBonusClick.innerText = bonusClickPrice
+displayAutoClick.innerText = autoClickPrice
+
+console.log(bonusActive)
+//les fonctions
+// function counterIncrement() {
+//     count++;         
+//     //count+= lvlBonusClickPlus - 1;
+//     counter.innerHTML = count;
+// }
+
+function buyBonusClick() {
+    if (count > bonusClickPrice){ 
+        bonusActive = 1;
+        count = count - bonusClickPrice;
+        counter.innerHTML = count;
+        bonusClickPrice = bonusClickPrice + 200;
+        lvlBonusClickPlus += 1
+        displayBonusClick.innerHTML = bonusClickPrice
+        displayBonusClickPlus.innerHTML = lvlBonusClickPlus 
+        
+    } else {
+        const message = document.getElementsByTagName('p')[0];
+        //message.insertBefore(message, message.nextSibling);
+        message.textContent = "Tu n'as pas assez de points";
+        //message.innerHTML = "Tu n'a pas assez de point !";
+        message.style.textAlign = 'center';
+        message.style.height = '16px';
+
+        
+        setTimeout(function() {
+            //message.remove();
+            message.textContent = "";
+          }, 2000);
+        
+        
+    }
 }
-   
 
-//je selectionne l'élement cible et affiche la valeur du compteur dessus
-let clk = document.querySelector('.clk');
-//document.querySelector('#compteur').innerHTML = count;
-
-
-// fonction qui incrémente de 1 le compteur
-function compteurIncrement() {
-   count = count + 1;
-   let displayCount = document.querySelector('#compteur');
-   displayCount.innerHTML = count;
+function counterIncrement() {
+    count++;         
+    //count+= lvlBonusClickPlus - 1;
+    counter.innerHTML = count;
+    if (bonusActive = 1) {
+        count+= lvlBonusClickPlus;
+    }
 }
 
-//fonction qui met a jour le compteur 
-let start = function start() {
-   setInterval(compteurIncrement(), 1000)
-   setInterval(localStorage.setItem("count",count),);
-}
+//le clicker 
+clicker.addEventListener("click",function(){
+    counterIncrement();
+})
 
-clk.addEventListener('click', start);
+//les bouttons achat
+btnBuyBonusClick.addEventListener("click",function(){
+    buyBonusClick();
+})
 
-//c'est pour faire afficher le compteur à l'actualisation de la page
-let displayCount = document.querySelector('#compteur');
-displayCount.innerText = localStorage.getItem("count");
